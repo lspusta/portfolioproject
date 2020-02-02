06347d81ccf7dd239b4c543d6e07bc1d7e7354fb
Martin Spusta <lucas.spusta@gmail.com>
2020-02-02 15:17:55 -0500

    Init Commit

---
+++ firebase.js
@@ -0,0 +1,14 @@
+function initFirebase(){
+  var firebaseConfig = {
+    apiKey: "AIzaSyDz-sSk3dDCZHWNDQE_8L6U7SN9bj5Ti0Y",
+    authDomain: "lucasportfolio-9bdb3.firebaseapp.com",
+    databaseURL: "https://lucasportfolio-9bdb3.firebaseio.com",
+    projectId: "lucasportfolio-9bdb3",
+    storageBucket: "lucasportfolio-9bdb3.appspot.com",
+    messagingSenderId: "772228300808",
+    appId: "1:772228300808:web:d679491540dca7b368d1bd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+}
